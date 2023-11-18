3fa3923bdf1471b4ebcdf9c75c1a9d402f318d46
Anytka492 <126511027+Anytka492@users.noreply.github.com>
2023-11-18 23:10:50 +0300

    Update urls.py

--- news/urls.py
+++ news/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import Article, ArticleId
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('news/', Article.as_view()),
-    path('news/<int:id>', ArticleId.as_view()),
+    path('', Article.as_view()),
+    path('int:pk>/', ArticleId.as_view()),
 ]
