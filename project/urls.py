00091bb87a20a5a86361f8467cc7b3fc24b71a97
Anytka492 <126511027+Anytka492@users.noreply.github.com>
2023-11-18 20:42:43 +0300

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from news.views import Article, ArticleId
+from .views import Article, ArticleId
 
 urlpatterns = [
     path('admin/', admin.site.urls),
